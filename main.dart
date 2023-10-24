406b899832a642822a0eedb735702461c687107b
Thiago <striohp@gmail.com>
2023-10-23 21:56:46 -0300

    primeiro commit

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main(){
+  String nome = 'Thiago';
+  int idade = 27;
+
+  if (idade >= 18) {
+    print ('$nome é maior de idade!');
+  }
+  else {
+    print ('$nome é menor de idade!');
+  }
+
+}
