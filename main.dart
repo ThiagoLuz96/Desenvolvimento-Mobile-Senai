253e7239b8ff30b84bb267a383e64c234eacb053
Thiago <striohp@gmail.com>
2023-10-24 21:35:27 -0300

    Commit da aula 24.10

--- main.dart
+++ main.dart
@@ -1,12 +1,36 @@
-void main(){
+import 'dart:io';
+
+void main() async {
+  //O async determina que um método será assíncrono, ou seja,
+  //não irá retornar algo imediatamente, então o aplicativo pode continuar a execução de outras tarefas enquanto o processamento não é finalizado.
   String nome = 'Thiago';
-  int idade = 27;
+
+  print('Insira sua idade:');
+
+  String? response = await stdin.readLineSync();
+  //O await serve para determinar que o aplicativo deve esperar uma resposta de uma função antes de continuar a execução.
+  //Isso é muito importante pois há casos em que uma função depende do retorno de outra.
+
+  //Já o Future determina que uma função irá retornar algo no “futuro”, ou seja, é uma função que levará um tempo até ser finalizada.
+
+  int idade = 0;
+
+  if (response != null) {
+    //response != null é verificador se o valor é nulo
+    idade = int.parse(response);
+  } else {
+    print('insira uma idade correta');
+    print('rode novamente');
+    return;
+  }
 
   if (idade >= 18) {
-    print ('$nome é maior de idade!');
+    print('$nome é Adulto!');
+  } else if (idade < 12 && idade > 10) {
+    print('$nome é pré-adolescente!');
+  } else if (idade < 18 && idade > 12) {
+    print('$nome é Adolescente!');
+  } else {
+    print('$nome é Criança!');
   }
-  else {
-    print ('$nome é menor de idade!');
-  }
-
 }
