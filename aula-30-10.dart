e1cf812d7bdb7a7f6d03492a95158a8b8ac05344
Thiago Lucas da Luz <107091141+ThiagoLuz96@users.noreply.github.com>
2023-10-30 21:54:52 -0300

    Add files via upload

---
+++ aula-30-10.dart
@@ -0,0 +1,24 @@
+void main() {
+/*  double imc = 0;
+  double peso = 50;
+  double altura = 1.80;
+
+  imc = peso/(altura*altura);
+  print('$imc Kg/m2');*/
+
+/*  for(int i=0; i<=10; i++){
+    print(i);
+  }
+*/
+
+/*int i = 0;
+int a = 0;
+for(; i<=10; i++){
+  a += i;
+  }
+  print(a);
+*/
+
+
+
+}
