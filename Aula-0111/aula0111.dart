7b0cddf7b0806ff1e78f0d02ec0e41cc53fb157b
Thiago Lucas da Luz <107091141+ThiagoLuz96@users.noreply.github.com>
2023-11-01 21:58:45 -0300

    Delete Aula-0111 directory

--- Aula-0111/aula0111.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-void main() {
-  print('digite um número');
-  int n = int.parse(stdin.readLineSync()!);
-  print(n);
-
-}
--- Aula-0111/ex012.dart
+++
@@ -1,8 +0,0 @@
-// Imprima os números de 10 a 1 usando um loop while
-void main(){
-  int i = 10;
-  while (i >= 0) {
-    print(i);
-    i--;
-  }
-}
